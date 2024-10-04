12a1383f669a67e08e75fd3e62bc0de81df55145
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-10-04 20:50:35 +0200

    Pridavam reseni

---
+++ script.js
@@ -0,0 +1,4 @@
+//1. Sestavte e-mailovou adresu tak, že sečtete dohromady vaše křestní jméno, znak tečka, vaše příjmení a koncovku @mujmail.com.
+//2. Celou e-mailovou adresu vypište do stránky.
+
+document.body.innerHTML +='Magdalena' + '.' + 'Stranska' + '@mujmail.com';
